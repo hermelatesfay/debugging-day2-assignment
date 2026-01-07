7a40898691bb7636daf99c0c61c9a2abd59507b1
hermelatesfay <hermelatesfay802@gmail.com>
2026-01-07 23:47:26 +0300

    eslint and prettier

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
+
+    // Simulated users
+    const users = {
+manager: { password: "1234", role: "manager" },
+agent: { password: "1234", role: "salesAgent" },
+director: { password: "1234", role: "director" }
+    };
+
+    if (users[username] && users[username].password === password) {
+localStorage.setItem("role", users[username].role);
+
+if (users[username].role === "manager") {
+window.location.href = "manager-dashboard.html";
+    } else if (users[username].role === "salesAgent") {
+window.location.href = "agent-dashboard.html";
+} else {
+        window.location.href = "director-dashboard.html";
+      }
+
+    } else {
+document.getElementById("errorMsg").textContent =
+"Invalid username or password";
+    }
+  });
