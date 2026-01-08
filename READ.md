5260d79a2f1875953995593500cb86165912c39a
hermelatesfay <hermelatesfay802@gmail.com>
2026-01-08 20:05:49 +0300

    modified

---
+++ READ.md
@@ -0,0 +1,8 @@
+                            Issues I fixed 
+        
+        -Using prettier, I was able to fix lines of code that were not indented well
+
+        -Using eslint, I was able to identify a variable name userName insted of username 
+        and how I fixed it is changed N to n in username
+
+        -A screenshot of before and after is within this folder called screenshot
--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-const username = document.getElementById("username").value.trim();
-const password = document.getElementById("password").value.trim();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-    // Simulated users
-    const users = {
-manager: { password: "1234", role: "manager" },
-agent: { password: "1234", role: "salesAgent" },
-director: { password: "1234", role: "director" }
-    };
+  // Simulated users
+  const users = {
+    manager: { password: "1234", role: "manager" },
+    agent: { password: "1234", role: "salesAgent" },
+    director: { password: "1234", role: "director" },
+  };
 
-    if (users[username] && users[username].password === password) {
-localStorage.setItem("role", users[username].role);
+  if (users[username] && users[username].password === password) {
+    localStorage.setItem("role", users[username].role);
 
-if (users[username].role === "manager") {
-window.location.href = "manager-dashboard.html";
-    } else if (users[username].role === "salesAgent") {
-window.location.href = "agent-dashboard.html";
-} else {
-        window.location.href = "director-dashboard.html";
-      }
-
+    if (users[username].role === "manager") {
+      window.location.href = "manager-dashboard.html";
+    } else if (users[username].Role === "salesAgent") {
+      window.location.href = "agent-dashboard.html";
     } else {
-document.getElementById("errorMsg").textContent =
-"Invalid username or password";
+      window.location.href = "director-dashboard.html";
     }
-  });
+  } else {
+    document.getElementById("errorMsg").textContent =
+      "Invalid username or password";
+  }
+});
